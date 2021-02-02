190f10498ae5d620c4f1d69f8b84361ba6795a68
Raghav-Deo <66122754+Raghav-Deo@users.noreply.github.com>
2021-02-02 14:50:55 +0000

    Structure updated

--- key-randomizer.py
+++ key-randomizer.py
@@ -1,9 +1,17 @@
-from Packages.main import key_randomizer, password_saver
-print("Coded and maintained by Raghav Deo")
-output = key_randomizer(input("Give your password here : "))
-print("Thanks for using our tool !")
-print(output)
-print("This is your key,this key will also be saved in a text file named as password.txt")
-print("Thanks for using our tool !")
-password_saver(output=output)
-quit()
+from Packages.main import key_randomizer
+
+
+if __name__ == '__main__':
+    key_randomizer.intro()
+    key_randomizer = key_randomizer(input("Give your password here : "))
+    output = key_randomizer.password_generator()
+    print(output+'\nThis is your key,this key will also be saved in a text file named as password.txt\nThanks for '
+                 'using our tool !\n(Ignore the text shown below)')
+    key_randomizer.process_completed()
+    key_randomizer.password_saver()
+    key_randomizer.password_saved()
+    quit()
+elif __name__ != '__main__':
+    print("You are not allowed to run this program by importing it\nExiting!")
+else:
+    print("An unknown error occurred while executing the program")
