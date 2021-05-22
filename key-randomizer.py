12d1ca7eb9e0aa3300febaa89abddac481367021
Ravbav Deo <66122754+Raghav-Deo@users.noreply.github.com>
2021-05-22 08:08:37 +0000

    Added comments

--- key-randomizer.py
+++ key-randomizer.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 
 
-from Packages.main import key_randomizer
+from Packages.main import Keyrandomizer  # importing the Keyrandomizer class
 
-
-if __name__ == '__main__':
-    key_randomizer.intro()
-    key_randomizer = key_randomizer(input("Give your password here : "))
-    output = key_randomizer.password_generator()
+if __name__ == '__main__':  # Check to disallow importing
+    Keyrandomizer.intro()  # simple introductory line
+    key_randomizer = Keyrandomizer(input("Give your password here : "))  # Taking input to pass arguments to 
+    # constructor method and creating the object 
+    output = key_randomizer.password_generator()  # saving the output to output variable 
     print(output+'\nThis is your key,this key will also be saved in a text file named as password.txt\nThanks for '
                  'using our tool !\n(Ignore the text shown below)')
-    key_randomizer.process_completed()
+    key_randomizer.process_completed()  # To play the audio file for process completion
     key_randomizer.password_saver()
-    key_randomizer.password_saved()
-    quit()
-elif __name__ != '__main__':
-    print("You are not allowed to run this program by importing it\nExiting!")
-else:
-    print("An unknown error occurred while executing the program")
+    key_randomizer.password_saved()  # To play the audio file for process completion
+    quit()  # Quit program after completing the process
+elif __name__ != '__main__':  # Check to disallow importing
+    print("You are not allowed to run this program by importing it\nExiting!")  # Message to be shown if imported
