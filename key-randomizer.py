6cf1a0845be1980fdcb2006def41cdec39ae8b23
Ravbav Deo <66122754+Raghav-Deo@users.noreply.github.com>
2021-05-22 08:18:44 +0000

    Update key-randomizer.py

--- key-randomizer.py
+++ key-randomizer.py
@@ -5,14 +5,14 @@ from Packages.main import Keyrandomizer  # importing the Keyrandomizer class
 
 if __name__ == '__main__':  # Check to disallow importing
     Keyrandomizer.intro()  # simple introductory line
-    key_randomizer = Keyrandomizer(input("Give your password here : "))  # Taking input to pass arguments to 
-    # constructor method and creating the object 
-    output = key_randomizer.password_generator()  # saving the output to output variable 
+    key_randomizer = Keyrandomizer(input("Give your password here : "))  # Taking input to pass arguments to
+    # constructor method and creating the object
+    output = key_randomizer.password_randomizer()  # saving the output to output variable
     print(output+'\nThis is your key,this key will also be saved in a text file named as password.txt\nThanks for '
                  'using our tool !\n(Ignore the text shown below)')
-    key_randomizer.process_completed()  # To play the audio file for process completion
+    key_randomizer.process_randomized_audio()  # To play the audio file for process completion
     key_randomizer.password_saver()
-    key_randomizer.password_saved()  # To play the audio file for process completion
+    key_randomizer.password_saved_audio()  # To play the audio file for process completion
     quit()  # Quit program after completing the process
 elif __name__ != '__main__':  # Check to disallow importing
     print("You are not allowed to run this program by importing it\nExiting!")  # Message to be shown if imported
